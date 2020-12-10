a7920baefba42f6be21a3a9ae49ac77daaba2e27
Daniel Rojas <daniel.drojas@outlook.com>
2020-12-10 13:50:32 -0500

    add controller and routes

---
+++ backend/controllers/playerControllers.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import { PlayerSchema } from '../models/playerModel';
+
+
+const Player = mongoose.model('Player', PlayerSchema);
+
+export const addNewPlayer = (req, res) => {
+    let newPlayer = new Player(req.body)
+
+    newPlayer.save((err, Player) => {
+        if (err) {
+            res.send(err)
+        }
+        res.json(Player)
+    })
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
+import routes from './routes/soccerRoutes';
 
 
 const app = express();
@@ -14,7 +15,10 @@ mongoose.connect('mongodb://localhost/soccerDB', {
 });
 
 //Body-Parser setup
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+routes(app)
 
 app.get('/', (req, res) => {
     res.send(`Our soccer application is running on ${PORT}`)
