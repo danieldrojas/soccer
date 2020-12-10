a1dd7f47f6a94b29d0d9193b4a550db104345b28
Daniel Rojas <daniel.drojas@outlook.com>
2020-12-10 13:27:06 -0500

    Mongodb setup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 
 const app = express();
 const PORT = 3000;
+
+//Mongo Connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/soccerDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//Body-Parser setup
+app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
     res.send(`Our soccer application is running on ${PORT}`)
